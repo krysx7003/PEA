d75b054947fd9c9ac176b87eb3e2755e038bde49
krysx7003 <krysx7003@gmail.com>
2024-11-27 19:44:59 +0100

    Filesystem refactor + CMake is working

---
+++ build/CMakeFiles/Proj_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj_1.dir/Proj_1/TSP1.cpp.obj"
+  "CMakeFiles/Proj_1.dir/Proj_1/TSP1.cpp.obj.d"
+  "CMakeFiles/Proj_1.dir/include/Graph.cpp.obj"
+  "CMakeFiles/Proj_1.dir/include/Graph.cpp.obj.d"
+  "Proj_1/Proj_1.exe"
+  "Proj_1/Proj_1.exe.manifest"
+  "Proj_1/Proj_1.pdb"
+  "libProj_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proj_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Proj_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Proj_1.
+# This may be replaced when dependencies are built.
