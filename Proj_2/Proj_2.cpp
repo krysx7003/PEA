cb23b749b617a9d798c7f330518755c761d23425
krysx7003 <krysx7003@gmail.com>
2024-12-02 00:32:45 +0100

    Fix for Proj_1 and basics of latex

---
+++ Proj_2/Proj_2.cpp
@@ -0,0 +1,9 @@
+//Biblioteka nlohmann/json służąca do obsługi pilków json
+//https://github.com/nlohmann/json
+#include "json.hpp"
+#include "Graph.hpp"
+#include "Node.hpp"
+
+int main(){
+    return 0;
+}
